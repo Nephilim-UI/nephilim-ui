3c45b9f25f8ba3437a34a4bd2c87782ddf7251dd
Anshuman Goswami <anshumangoswami07.com>
2023-10-04 15:24:25 +0530

    feature/ tailwind-css, testing

--- src/react/components/Button/button.tsx
+++ src/react/components/Button/button.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react'
+import '../../../styles/global.css'
 
-type Props = {};
+type Props = {
+  children: React.ReactNode
+}
 
-export default function button({}: Props) {
-  return <div>click me</div>;
+export default function button({ children }: Props) {
+  return (
+    <button className='bg-neutral-300 text-slate-800 font-semibold font-mono px-2 py-1 border-none outline-none rounded-md text-sm'>
+      {children}
+    </button>
+  )
 }

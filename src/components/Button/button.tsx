6dfe92decf769f3cc98d15c2744f74a372f643e3
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-10-08 14:52:13 +0530

    fix/ styles

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '/styles/global.css'
+import '../../styles/global.css'
 import { ButtonProps } from '../../../types/button.types'
 import { color_varient } from './button.styles'
 

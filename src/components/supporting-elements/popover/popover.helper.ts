b14517fe239e59f1605ddee6d53c14679cb66664
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-10-17 16:43:06 +0530

    fix: popover

--- src/components/supporting-elements/popover/popover.helper.ts
+++ src/components/supporting-elements/popover/popover.helper.ts
@@ -1,9 +1,6 @@
 import { cva } from 'class-variance-authority'
 
-export const PopoverVariant = cva(
-  ['relative', 'ml-[40vw]', 'mt-[20vh]', 'w-max', 'flex', 'item-center', 'justify-center'],
-  {
-    variants: {},
-    defaultVariants: {},
-  },
-)
+export const PopoverVariant = cva(['relative', 'w-max', 'flex', 'item-center', 'justify-center'], {
+  variants: {},
+  defaultVariants: {},
+})
